c4edc628806b624da56a761739ccfbee270bf14e
Jaime <jaime251102@gmail.com>
2022-03-23 12:01:42 -0400

    Add project files.

---
+++ 2doParcialPro3/Data/Robo.cs
@@ -0,0 +1,14 @@
+﻿namespace _2doParcialPro3.Data
+{
+    public class Robo
+    {
+        public string Cedula { get; set; } 
+        public string Nombre { get; set; } 
+        public string Fecha { get; set; } 
+        public string Item { get; set; } 
+        public string Valor { get; set; } 
+        public string Lugar { get; set; } 
+        public string Latitud { get; set; } 
+        public string Longitud { get; set; }
+    }
+}
